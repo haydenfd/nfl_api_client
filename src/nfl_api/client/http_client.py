edcb7f8d20d6d0e05cb50f8822aaf7e48fe276a9
Hayden D’Souza <haydenfds@gmail.com>
2025-06-03 20:11:49 -0700

    Added basic docs for player season stats response w/ return stat names, descriptions Exposed lib and clients w/ __init__

--- src/nfl_api/client/http_client.py
+++ src/nfl_api/client/http_client.py
@@ -21,6 +21,7 @@ class HttpClient:
     def get(self, endpoint, params=None):
         url = f"{self.base_url}/{endpoint.lstrip('/')}"
         with httpx.Client(headers=self.headers, timeout=self.timeout) as client:
+            print(url)
             resp = client.get(url, params=params)
             resp.raise_for_status()
             return resp.json()
@@ -34,48 +35,77 @@ class HttpClient:
             responses = await asyncio.gather(*tasks)
             print(f"Fetching for {len(tasks)} different tasks")
             return [res.json() for res in responses]
+        
+def extract_stat_category(data, category_name):
+    try:
+        categories = data["splits"]["categories"]
+        target = next(cat for cat in categories if cat["name"] == category_name)
+        return {stat["name"]: [stat["value"], stat['description']]for stat in target["stats"]}
+    except (KeyError, StopIteration):
+        return {}
 
 
-# def main():
-#     start_time = time.perf_counter()
+def get_player_stats(client: HttpClient, endpoint: str):
+    data = client.get(endpoint)
+    return {
+        "general": extract_stat_category(data, "general"),
+        "passing": extract_stat_category(data, "passing"),
+        "rushing": extract_stat_category(data, "rushing"),
+        "receiving": extract_stat_category(data, "receiving"),
+        "defensive": extract_stat_category(data, "defensive"),
+        "defensiveInterceptions": extract_stat_category(data, "defensiveInterceptions"),
+        "scoring": extract_stat_category(data, "scoring"),
+    }
 
-#     client = HttpClient(
-#         base_url="https://sports.core.api.espn.com",
-#     )
+    
+# Code for testing execution time of simple gets. 
+def main():
+    start_time = time.perf_counter()
 
-#     data = client.get("/v2/sports/football/leagues/nfl/athletes/3139477/statisticslog")
+    client = HttpClient(
+        base_url="https://sports.core.api.espn.com",
+    )
 
-#     end_time = time.perf_counter()
-#     elapsed = end_time - start_time
-
-#     print(f"\nTime taken: {elapsed:.3f} seconds")
-#     print(data)
+    # data = client.get("/v2/sports/football/leagues/nfl/athletes/3139477/statisticslog")
+    endpoint = "/v2/sports/football/leagues/nfl/seasons/2024/types/2/athletes/3139477/statistics/0"
+    player_stats = get_player_stats(client, endpoint)
+    stats = get_player_stats(client, endpoint)
+    for category, stat_dict in stats.items():
+            print(f"\n=== {category.upper()} STATS ===")
+            if stat_dict:
+                for stat_name, val in stat_dict.items():
+                    print(f"{stat_name:30} : {val[1]}")
+            else:
+                print("No data available.")
+    end_time = time.perf_counter()
+    elapsed = end_time - start_time
+    print(f"\nTime taken: {elapsed:.3f} seconds")
+    # print(data)
 
 # if __name__ == "__main__":
 #     main()
 
-def main():
-    client = HttpClient(
-        base_url="https://sports.core.api.espn.com", 
-    )
-    
-    endpoints = [
-        "v2/sports/football/leagues/nfl/seasons/2024/types/2/athletes/3139477/statistics/0?lang=en&region=us",
-        "v2/sports/football/leagues/nfl/seasons/2023/types/2/athletes/3139477/statistics/0?lang=en&region=us",
-        "v2/sports/football/leagues/nfl/seasons/2022/types/2/athletes/3139477/statistics/0?lang=en&region=us",
-        "v2/sports/football/leagues/nfl/seasons/2021/types/2/athletes/3139477/statistics/0?lang=en&region=us",
-        "v2/sports/football/leagues/nfl/seasons/2020/types/2/athletes/3139477/statistics/0?lang=en&region=us",
-        "v2/sports/football/leagues/nfl/seasons/2019/types/2/athletes/3139477/statistics/0?lang=en&region=us",
-        "v2/sports/football/leagues/nfl/seasons/2018/types/2/athletes/3139477/statistics/0?lang=en&region=us",
-        "v2/sports/football/leagues/nfl/seasons/2017/types/2/athletes/3139477/statistics/0?lang=en&region=us",
-    ]
+# def main():
+#     client = HttpClient(
+#         base_url="https://sports.core.api.espn.com", 
+#     )
 
-    start = time.perf_counter()
-    results = asyncio.run(client.get_multiple_urls_concurrent(endpoints))
-    end = time.perf_counter()
+#     endpoints = [
+#         "v2/sports/football/leagues/nfl/seasons/2024/types/2/athletes/3139477/statistics/0?lang=en&region=us",
+#         "v2/sports/football/leagues/nfl/seasons/2023/types/2/athletes/3139477/statistics/0?lang=en&region=us",
+#         "v2/sports/football/leagues/nfl/seasons/2022/types/2/athletes/3139477/statistics/0?lang=en&region=us",
+#         "v2/sports/football/leagues/nfl/seasons/2021/types/2/athletes/3139477/statistics/0?lang=en&region=us",
+#         "v2/sports/football/leagues/nfl/seasons/2020/types/2/athletes/3139477/statistics/0?lang=en&region=us",
+#         "v2/sports/football/leagues/nfl/seasons/2019/types/2/athletes/3139477/statistics/0?lang=en&region=us",
+#         "v2/sports/football/leagues/nfl/seasons/2018/types/2/athletes/3139477/statistics/0?lang=en&region=us",
+#         "v2/sports/football/leagues/nfl/seasons/2017/types/2/athletes/3139477/statistics/0?lang=en&region=us",
+#     ]
 
-    print(f"\nFetched {len(results)} stats in {end - start:.3f} seconds")
-    # print(results)
+#     start = time.perf_counter()
+#     results = asyncio.run(client.get_multiple_urls_concurrent(endpoints))
+#     end = time.perf_counter()
+
+#     print(f"\nFetched {len(results)} stats in {end - start:.3f} seconds")
 
 if __name__ == "__main__":
     main()
