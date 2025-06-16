2cf2ca7c37dd0b060a6648330f21d0a4d91ca218
Hayden D’Souza <haydenfds@gmail.com>
2025-06-16 13:38:18 -0700

    Updated endpoint registry

--- src/nfl_api_client/lib/endpoint_registry.py
+++ src/nfl_api_client/lib/endpoint_registry.py
@@ -1,6 +1,19 @@
+SITE_URL = "https://site.api.espn.com/apis/site/v2/sports/football/nfl"
+WEB_URL = "https://site.web.api.espn.com/apis/common/v3/sports/football/nfl"
+CORE_URL = "https://sports.core.api.espn.com/v2/sports/football/leagues/nfl"
+
+
+#fmt: off
 ENDPOINT_REGISTRY = {
-    "TEAM_SCHEDULE": "https://site.api.espn.com/apis/site/v2/sports/football/nfl/teams/{team_id}/schedule",
-    "TEAM_ROSTER": "https://site.api.espn.com/apis/site/v2/sports/football/nfl/teams/{team_id}/roster",
-    "SEASON_STANDINGS": "https://sports.core.api.espn.com/v2/sports/football/leagues/nfl/seasons/{year}/types/{season_type}/groups/{conference_type}/standings/0",
-    "TEAM_DEPTH_CHART": "https://sports.core.api.espn.com/v2/sports/football/leagues/nfl/seasons/{year}/teams/{team_id}/depthcharts"
+    "TEAM_SCHEDULE":                    f"{SITE_URL}/teams/{{team_id}}/schedule",
+    "TEAM_ROSTER":                      f"{SITE_URL}/teams/{{team_id}}/roster",
+    "SEASON_STANDINGS":                 f"{CORE_URL}/seasons/{{year}}/types/{{season_type}}/groups/{{conference_type}}/standings/0",
+    "TEAM_DEPTH_CHART":                 f"{CORE_URL}/seasons/{{year}}/teams/{{team_id}}/depthcharts",
+    "STAT_LEADERS":                     f"{CORE_URL}/seasons/{{year}}/types/{{season_type}}/leaders?limit={{limit}}",
+    "PLAYER_CAREER_STATS":              f"{CORE_URL}/athletes/{{player_id}}/statisticslog",
+    "PLAYER_CAREER_BASIC_STATS":        f"{WEB_URL}/athletes/{{player_id}}/stats?seasontype={{season_type}}",
+    "LEAGUE_GAME_FINDER":               f"{SITE_URL}/scoreboard",
+    "DRAFT_SUMMARY":                    f"{CORE_URL}/seasons/{{year}}/draft/rounds",
+    "GAME_SUMMARY":                     f"{SITE_URL}/summary?event={{game_id}}"
 }
+# fmt: on
