10461ebecdad33808cd1bcebdf10508c843a8d1c
Hayden D’Souza <haydenfds@gmail.com>
2025-06-19 22:41:00 -0700

    Added demo files

--- examples/team_roster.py
+++ examples/team_roster.py
@@ -1,6 +1,6 @@
 from nfl_api_client.endpoints.team_roster import TeamRoster
-
+from nfl_api_client.lib.parameters import TeamID
 if __name__ == "__main__":
-    info = TeamRoster("12")
-    x = info.get_df()
-    print(x)
+    roster = TeamRoster(32)
+    df = roster.get_dataset("TEAM_ROSTER").get_data_frame()
+    print(df)
