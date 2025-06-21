70293a0814f230a7359d9d4964ed2d50065cf689
Hayden D’Souza <haydenfds@gmail.com>
2025-06-21 06:21:48 -0700

    Added util func for extracting any id given a particular ref. Generalizable for athletes, events, season, teams etc

--- src/nfl_api_client/lib/utils.py
+++ src/nfl_api_client/lib/utils.py
@@ -1,6 +1,16 @@
-# src/nfl_api/lib/utils.py
-
+import re
+from typing import Optional 
 from datetime import datetime
 
 def format_date_str(date_str):
     return datetime.strptime(date_str, "%Y-%m-%dT%H:%MZ").strftime("%m-%d-%Y")
+
+
+def extract_id_from_ref(ref_url: str, key: str) -> Optional[int]:
+    """ 
+    Receives a $ref url and a given key ("seasons", "teams", "athletes")
+    Returns the corresponding id value for the key after parsing through the ref_url
+    """
+    pattern = rf"{re.escape(key)}/(\d+)"
+    match = re.search(pattern, ref_url)
+    return int(match.group(1)) if match else None
