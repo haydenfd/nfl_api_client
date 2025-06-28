61a49e8bded3ad3f0379c9536a2757a5e7dab0d3
Hayden D’Souza <haydenfds@gmail.com>
2025-06-27 23:58:33 -0700

    Added proper examples for team roster, debut year is a bit of a bogus field, throw it out Understood that rookies may have age and dob show up as None, only for some though not for all. This obviously needs to be updated by ESPN closer to when the season approaches

--- examples/team_roster.py
+++ examples/team_roster.py
@@ -1,6 +1,18 @@
 from nfl_api_client.endpoints.team_roster import TeamRoster
 from nfl_api_client.lib.parameters import TeamID
-if __name__ == "__main__":
-    roster = TeamRoster(32)
-    df = roster.get_dataset("TEAM_ROSTER").get_dataframe()
-    print(df)
+
+
+_roster = TeamRoster(team_id=12)
+print(_roster.get_url())
+chiefs_roster = _roster.get_dataset("TEAM_ROSTER").get_dataframe()
+# print(chiefs_roster.get_dataset("TEAM_ROSTER").get_dataframe())
+subset = chiefs_roster[["full_name", "college"]]
+
+missing_info = subset
+
+print(missing_info)
+
+# debut year is useless. 
+# Experience is for how many years since they've been in the league. 
+# age needs to be typecast to int. Cannot be because of pandas quirk in dataframes
+# age shows up as NaN and dob None for rookies
